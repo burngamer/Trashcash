8d30a8ab26c6332fc3ab1571de173cb869b986c1
Kostas <Kostas@email.com>
2025-07-20 11:38:41 +0200

    Items and related Items + a few details about related items

--- trashcash/trashcash/urls.py
+++ trashcash/trashcash/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
